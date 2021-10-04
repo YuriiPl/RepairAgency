9b31c328240dce6e24470cb389a5e1cf405cd889
YuriiPl <YuriiPl@fake.com>
2021-10-04 20:25:15 +0300

    Begin (Empty project)

---
+++ RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/RepairAgencySpringApplication.java
@@ -0,0 +1,13 @@
+package com.repairagency.repairagencyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairAgencySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairAgencySpringApplication.class, args);
+    }
+
+}
---
+++ RepairAgencySpring/src/test/java/com/repairagency/repairagencyspring/RepairAgencySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repairagency.repairagencyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairAgencySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
