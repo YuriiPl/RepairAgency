52bcef699a733226e4cdc846c9bba80463c19912
YuriiPl <noemail@noemail>
2021-10-06 01:38:11 +0300

    Add "today" injection

---
+++ RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/config/VariablesInjectionConfig.java
@@ -0,0 +1,38 @@
+package com.repairagency.repairagencyspring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+@Configuration
+public class VariablesInjectionConfig implements WebMvcConfigurer {
+    final LocaleResolver localeResolver;
+
+    @Autowired
+    public VariablesInjectionConfig(LocaleResolver localeResolver) {
+        this.localeResolver = localeResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+                if(modelAndView==null)return;
+                DateTimeFormatter frDateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(localeResolver.resolveLocale(request));
+                String frFormattedDate = LocalDate.now().format(frDateFormatter);
+                modelAndView.addObject("today",frFormattedDate);
+            }
+        });
+    }
+}
