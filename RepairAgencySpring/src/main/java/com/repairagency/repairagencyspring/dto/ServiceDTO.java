bf8a78797ea850ff1e96b516a53b2174d9d6bc24
YuriiPl <YuriiPl@fake.com>
2021-10-09 08:27:19 +0300

    ServiceDTO

---
+++ RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/dto/ServiceDTO.java
@@ -0,0 +1,31 @@
+package com.repairagency.repairagencyspring.dto;
+
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class ServiceDTO {
+
+    @NotBlank(message = "wrongName")
+    @Pattern(regexp = "^(?! )(?!.* $)(?!(?:.* ){2})[\\da-zA-Zа-щьюяіїєґА-ЩЮЯІЇЄҐ ']{2,128}$", message = "wrongName")
+    private String serviceName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o.getClass().getName().equals(this.getClass().getName()) )) return false;
+        ServiceDTO service = (ServiceDTO) o;
+        return serviceName.equals(service.serviceName);
+    }
+
+    @Override
+    public int hashCode() {
+        return serviceName.hashCode();
+    }
+}
--- RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/entity/Service.java
+++ RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/entity/Service.java
@@ -1,22 +1,44 @@
 package com.repairagency.repairagencyspring.entity;
 
+import com.repairagency.repairagencyspring.dto.ServiceDTO;
 import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
-@Setter
-@AllArgsConstructor
+//@EqualsAndHashCode(of = {"name"})
+@ToString(of = {"name"})
 @NoArgsConstructor
-@Builder
-@ToString
-@Entity(name="Service")
+@Entity
+@Table(name = "Service")
 public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", unique = true)
+    //name varchar(128) not null unique
+    @Column(name = "name", unique = true, nullable = false, length = 128)
     private String name;
+
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public Service(ServiceDTO serviceDTO) {
+        name = serviceDTO.getServiceName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Service)) return false;
+        Service service = (Service) o;
+        return name.equals(service.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
