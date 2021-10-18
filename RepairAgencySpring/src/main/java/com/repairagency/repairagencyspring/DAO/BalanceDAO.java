b23993d27dd763a7832d4f19eca9c0622e33dc3f
YuriiPl <YuriiPl@fake.com>
2021-10-18 23:23:46 +0300

    Add balance top-up for other to manager account

--- RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/DAO/BalanceDAO.java
+++ RepairAgencySpring/src/main/java/com/repairagency/repairagencyspring/DAO/BalanceDAO.java
@@ -1,22 +1,15 @@
-package com.repairagency.repairagencyspring.model.DAO;
+package com.repairagency.repairagencyspring.DAO;
 
-import com.repairagency.repairagencyspring.entity.UserAccount;
 import com.repairagency.repairagencyspring.entity.UserDB;
-import com.repairagency.repairagencyspring.repos.UserAccountRepository;
 import com.repairagency.repairagencyspring.repos.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 @Service
 @Slf4j
 public class BalanceDAO {
-
 
     private final UserRepository userRepository;
 
@@ -26,7 +19,7 @@ public class BalanceDAO {
     }
 
     @Transactional
-    public void addAmount(String login, long amount) throws BalanceTransactionException {
+    public long addAmount(String login, long amount) throws BalanceTransactionException {
         log.warn("TRANSACTION STARTS");
         UserDB user = userRepository.findByLogin(login).get();
         if (user == null) {
@@ -35,6 +28,7 @@ public class BalanceDAO {
         long newBalance = user.getAccount().getAmount() + amount;
         user.getAccount().setAmount(newBalance);
         log.warn("TRANSACTION FINISHES");
+        return newBalance;
     }
 
 
